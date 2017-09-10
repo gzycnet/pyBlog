22f7e0aa800bee9a1415885ce74a2c2a84793ec3
zhaoyoucai <zhaoyc128@163.com>
2017-09-10 18:31:52 +0800

    V1.0 实现基本的注册，用户审核，文章发布，前台展示，分类、标签等基本功能

---
+++ static/test.js
@@ -0,0 +1,7 @@
+/**
+ * Created by zhaoyoucai on 16/9/18.
+ */
+var myDate = new Date();
+document.write(myDate.getYear() + ' ' + myDate.getDay() + '' + myDate.getHourMinuteSecond());
+
+alert('aaaa');
---
+++ web.py
@@ -0,0 +1,572 @@
+#coding:utf-8
+import os.path
+import textwrap
+
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+
+import datetime
+
+from bson.objectid import ObjectId
+import hashlib
+
+
+from databaseCase import *
+
+import sys
+reload(sys)
+sys.setdefaultencoding( "utf-8" )
+
+
+from tornado.options import define, options
+define("port", default=8800, help="run on the given port", type=int)
+
+
+class BaseHandler(tornado.web.RequestHandler):
+    def get_current_user(self):
+        return self.get_secure_cookie("account")
+
+    def write_error(self, status_code, **kwargs):
+        if status_code == 404:
+            self.render('error/404.html')
+        elif status_code == 500:
+            self.render('error/500.html')
+        else:
+            self.write('error:' + str(status_code))
+
+
+class IndexHandler(BaseHandler):
+    def get(self):
+
+        mongo = MongoCase()
+        mongo.connect()
+        client = mongo.client
+
+        db = client.pyblog
+        articles = db.post.find().sort("date",-1).limit(20)
+
+
+        tags = db.tags.find().limit(50)
+        cateList = db.category.find()
+
+        cateData = dict()
+        cateData['menu'] = []
+        cateData['side'] = []
+        for c in cateList:
+            if c['showMenu']:
+               cateData['menu'].append(c)
+            if c['showSide']:
+                cateData['side'].append(c)
+
+
+        self.render('blog/index.html',articles=articles,tags=tags,cateData=cateData)
+
+        #self.render('index.html')
+
+    def write_error(self, status_code, **kwargs):
+        self.write("Gosh darnit, user! You caused a %d error.\n" % status_code)
+
+
+
+class BlogHandler(BaseHandler):
+    def get(self):
+
+        mongo = MongoCase()
+        mongo.connect()
+        client = mongo.client
+        db = client.pyblog
+        articles = db.post.find().sort("date",-1).limit(20)
+
+
+        tags = db.tags.find().limit(50)
+        cateList = db.category.find()
+
+        cateData = dict()
+        cateData['menu'] = []
+        cateData['side'] = []
+        for c in cateList:
+            if c['showMenu']:
+               cateData['menu'].append(c)
+            if c['showSide']:
+                cateData['side'].append(c)
+
+
+        self.render('blog/index.html',articles=articles,tags=tags,cateData=cateData)
+
+        #self.render('index.html')
+
+    def write_error(self, status_code, **kwargs):
+        self.write("Gosh darnit, user! You caused a %d error.\n" % status_code)
+
+
+
+class BlogCategoryHandler(BaseHandler):
+    def get(self,id):
+
+        pageSize = 20
+
+        try:
+            page = int(self.get_argument('page',1))
+        except:
+            page = 1
+
+        mongo = MongoCase()
+        mongo.connect()
+        client = mongo.client
+        db = client.pyblog
+        posts = db.post
+
+        option = {}
+
+        if id !='':
+            option = {"cate.id":ObjectId(id)}
+
+        totalCount = posts.find(option).count()
+
+        articles = posts.find(option).sort("date",-1).limit(pageSize).skip((page-1)*pageSize)
+
+        pageInfo = dict()
+        p = divmod(totalCount,pageSize)
+
+        totalPage = p[0]
+        if p[1]>0:
+            totalPage += 1
+
+        pageInfo['totalPage'] = totalPage
+        pageInfo['totalCount'] = totalCount
+        pageInfo['pageSize'] = pageSize
+        pageInfo['pageNo'] = page
+        pageInfo['pageList'] = range(1,totalPage+1)
+
+
+
+        tags = db.tags.find().limit(50)
+        cateList = db.category.find()
+
+        cateData = dict()
+        cateData['menu'] = []
+        cateData['side'] = []
+        for c in cateList:
+            if c['showMenu']:
+               cateData['menu'].append(c)
+            if c['showSide']:
+                cateData['side'].append(c)
+
+
+        self.render('blog/index.html',articles=articles,pageInfo=pageInfo,tags=tags,cateData=cateData)
+
+
+
+class BlogTagsHandler(BaseHandler):
+    def get(self,t):
+
+        pageSize = 20
+
+        try:
+            page = int(self.get_argument('page',1))
+        except:
+            page = 1
+
+        mongo = MongoCase()
+        mongo.connect()
+        client = mongo.client
+        posts = db.post
+
+        option = {}
+
+        if t !='':
+            option = {"tags":t}
+
+        totalCount = posts.find(option).count()
+
+        articles = posts.find(option).sort("date",-1).limit(pageSize).skip((page-1)*pageSize)
+
+        pageInfo = dict()
+        p = divmod(totalCount,pageSize)
+
+        totalPage = p[0]
+        if p[1]>0:
+            totalPage += 1
+
+        pageInfo['totalPage'] = totalPage
+        pageInfo['totalCount'] = totalCount
+        pageInfo['pageSize'] = pageSize
+        pageInfo['pageNo'] = page
+        pageInfo['pageList'] = range(1,totalPage+1)
+
+
+
+        tags = db.tags.find().limit(50)
+        cateList = db.category.find()
+
+        cateData = dict()
+        cateData['menu'] = []
+        cateData['side'] = []
+        for c in cateList:
+            if c['showMenu']:
+               cateData['menu'].append(c)
+            if c['showSide']:
+                cateData['side'].append(c)
+
+        self.render('blog/index.html',articles=articles,pageInfo=pageInfo,tags=tags,cateData=cateData)
+
+
+class PostArticleHandler(BaseHandler):
+    @tornado.web.authenticated
+    def get(self):
+        user=self.current_user
+        mongo = MongoCase()
+        mongo.connect()
+        client = mongo.client
+        db = client.pyblog
+        acc = db.user.find_one({"account":user})
+        if acc and int(acc['status'])<2 :
+
+            self.render('error.html',msg=u'您的账号待审核，还不能发布文章！')
+
+        else:
+            cateList = db.category.find()
+            self.render('post.html',referer = self.request.path,user=self.current_user,userInfo=acc,cateList=cateList)
+
+
+
+    def post(self):
+        #author = self.get_argument('author')
+        author = self.current_user
+        title = self.get_argument('title')
+        content = self.get_argument('content')
+        description = self.get_argument('description')
+        tags = self.get_argument('tags')
+        tags = tags.split(',')
+        cate = self.get_argument('cate','None')
+
+        mongo = MongoCase()
+        mongo.connect()
+        client = mongo.client
+
+        db = client.pyblog
+        posts = db.post
+        if cate != 'None':
+            t = db.category.find_one(ObjectId(cate))
+            new_post = {"author":author,"title":title,'content':content,'description':description,'tags':tags,'date':datetime.datetime.now(),'updateDate':datetime.datetime.now(),'status':0,'cate':{'id':ObjectId(cate),'name':t['name']},'views':0,'likes':0,'unlikes':0}
+        else:
+            new_post = {"author":author,"title":title,'content':content,'description':description,'tags':tags,'date':datetime.datetime.now(),'updateDate':datetime.datetime.now(),'status':0,'cate':None,'views':0,'likes':0,'unlikes':0}
+        posts.insert(new_post)
+
+        for t in tags:
+            if t != '':
+                try:
+                    db.tags.insert({'name':t})
+                except:
+                    pass
+
+        #self.render('index.html')
+        self.redirect('/ucenter/list')
+
+
+
+class ArticleDetailHandler(BaseHandler):
+    def get(self,id):
+        mongo = MongoCase()
+        mongo.connect()
+        client = mongo.client
+
+        db = client.pyblog
+        article = db.post.find_one({'_id':ObjectId(id)})
+
+        count = 0
+        if article.has_key('views'):
+            count += article['views']
+
+        db.post.update({'_id':ObjectId(id)},{'$set':{'views':count+1}})
+
+        tags = db.tags.find().limit(50)
+        cateList = db.category.find()
+
+        cateData = dict()
+        cateData['menu'] = []
+        cateData['side'] = []
+        for c in cateList:
+            if c['showMenu']:
+               cateData['menu'].append(c)
+            if c['showSide']:
+                cateData['side'].append(c)
+
+        self.render('blog/article.html',article=article,tags=tags,cateData=cateData)
+
+
+
+class AuthorArticleListHandler(BaseHandler):
+    @tornado.web.authenticated
+    def get(self):
+        user=self.current_user
+        mongo = MongoCase()
+        mongo.connect()
+        client = mongo.client
+        db = client.pyblog
+        userInfo = db.user.find_one({"account":user})
+        if userInfo:
+
+            articles = db.post.find({"author":user})
+            statusList = ['未审核','待修改','待审核','拒绝','通过']
+
+            self.render('list.html',articles=articles,referer = self.request.path,statusList=statusList,userInfo=userInfo)
+
+        else:
+            self.render('error.html',msg=u'无权限！ >> <a href="/account/login">登录</a>')
+
+
+class EditArticleHandler(BaseHandler):
+    @tornado.web.authenticated
+    def get(self,id):
+        user=self.current_user
+        mongo = MongoCase()
+        mongo.connect()
+        client = mongo.client
+        db = client.pyblog
+        article = db.post.find_one({'_id':ObjectId(id)})
+
+        userInfo = db.user.find_one({"account":user})
+        if article['author']!= user:
+            msg = u'无权编辑'
+            self.render('error.html',msg=msg)
+        else:
+            cateList = db.category.find()
+            self.render('edit.html',article=article,referer = self.request.path,cateList=cateList,userInfo=userInfo)
+
+
+    @tornado.web.authenticated
+    def post(self,*args, **kwargs):
+        id = self.get_argument('id')
+        title = self.get_argument('title')
+        description = self.get_argument('description')
+        content = self.get_argument('content')
+        tags = self.get_argument('tags','')
+        oldTags = self.get_argument('oldTags','')
+        mark = 0
+        if oldTags != tags:
+            mark +=1
+        tags = tags.strip(',').split(',')
+        cate = self.get_argument('cate')
+
+        mongo = MongoCase()
+        mongo.connect()
+        client = mongo.client
+        db = client.pyblog
+        article = db.post.find_one({'_id':ObjectId(id)})
+
+        if article['cate'] == None and cate !='None':
+            t = db.category.find_one(ObjectId(cate))
+            db.post.update({'_id':ObjectId(id)},
+                           {'$set':{
+                               'title':title,'content':content,'description':description,'tags':tags,'updateDate':datetime.datetime.now(),'status':2,'cate':{'id':ObjectId(cate),'name':t['name']}
+                           }
+                           })
+        else:
+            db.post.update({'_id':ObjectId(id)},
+                           {'$set':{
+                               'title':title,'content':content,'description':description,'tags':tags,'updateDate':datetime.datetime.now(),'status':2
+                           }
+                           })
+
+        if mark >0:
+            for t in tags:
+                if t != '':
+                    try:
+                        db.tags.insert({'name':t})
+                    except:
+                        pass
+
+
+        self.redirect('/ucenter/list')
+
+
+
+class AdminHandler(BaseHandler):
+    @tornado.web.authenticated
+    def get(self):
+        user = user=self.current_user
+        mongo = MongoCase()
+        mongo.connect()
+        client = mongo.client
+        db = client.pyblog
+        userInfo = db.user.find({"account":user})
+        if user and userInfo.count():
+            if userInfo[0]['isSupper']:
+                userList = db.user.find({"isSupper":False})
+
+                self.render('admin.html',userList=userList,userInfo=userInfo[0])
+            else:
+                self.redirect('/')
+        else:
+            self.clear_cookie('account')
+            self.render('error.html',msg=u'非法用户！')
+
+
+class AuditUserHandler(BaseHandler):
+    @tornado.web.authenticated
+    def get(self):
+        user = user=self.current_user
+        action = self.get_argument('action','')
+        id = self.get_argument('id','')
+        mongo = MongoCase()
+        mongo.connect()
+        client = mongo.client
+        db = client.pyblog
+        userInfo = db.user.find({'account':user})
+        if action != '' and id != ''and userInfo.count>0:
+            if userInfo[0]['isSupper']:
+                if action == 'delete':
+                    db.user.remove({'_id':ObjectId(id)})
+                    db.profile.remove({'_id':ObjectId(id)})
+                elif action == 'deActive':
+                    db.user.update({'_id':ObjectId(id)},{'$set':{'isActive':False,'status':1}})
+
+                elif action == 'active':
+                    db.user.update({'_id':ObjectId(id)},{'$set':{'isActive':True}})
+                elif action == 'audit':
+                    db.user.update({'_id':ObjectId(id)},{'$set':{'isActive':True,'status':2}})
+                self.redirect('/admin')
+            else:
+                self.render('error.html',msg=u'无权限删除！')
+        else:
+            self.render('error.html',msg=u'非法操作！')
+
+
+
+class RegHandler(BaseHandler):
+
+    def get(self):
+        user = self.current_user
+        referer = self.get_argument('referer','/')
+        if user:
+            self.redirect(referer)
+        else:
+            self.render('reg.html',referer=referer)
+
+    def post(self):
+        email = self.get_argument('email','')
+        password = self.get_argument('password','')
+        password_repeat = self.get_argument('password_repeat','')
+        type = self.get_argument('type','1')
+        if email == '' or '-' in email:
+            self.render('error.html',msg=u'非法账号 不能为空、且不能含有[-]')
+
+        elif password == '' or password_repeat =='':
+            self.render('error.html',msg=u'密码不能为空，且要6-18位!')
+        elif password != password_repeat:
+            self.render('error.html',msg=u'密码不一致！')
+
+        #查询
+        else:
+            mongo = MongoCase()
+            mongo.connect()
+            client = mongo.client
+            db = client.pyblog
+
+            if db.user.find({"account":email}).count()>0:
+                msg = email+u'已经注册过了'
+                msg += u'<a href="/login">登录</a>'
+                self.render('error.html',msg=msg)
+
+            else:
+
+                m = hashlib.md5()
+                m.update(password)
+                sign = m.hexdigest()
+                mongo = MongoCase()
+                mongo.connect()
+                client = mongo.client
+                db = client.pyblog
+                item = {"account":email,"username":email, "password":sign,"regDate":datetime.datetime.now(),'status':'0','type':type,
+                        "lastLoginDate":datetime.datetime.now(),"isActive":False,'isSupper':False}
+                db.user.insert(item)
+
+                self.set_secure_cookie("account", email)
+
+                self.redirect('/ucenter/list')
+
+
+class LoginHandler(BaseHandler):
+
+    def get(self):
+        user = self.current_user
+        referer = self.get_argument('next','/')
+        if user:
+            self.redirect(referer)
+        else:
+            self.render('login.html',referer=referer)
+
+
+    def post(self):
+
+        email = self.get_argument('email','')
+        password = self.get_argument('password','')
+
+        if email == '' or password == '':
+            self.render('error.html',msg=u'Email 密码 不能为空!')
+
+        else:
+            mongo = MongoCase()
+            mongo.connect()
+            client = mongo.client
+            db = client.pyblog
+            user = db.user.find({"account":email})
+            if user.count()<1:
+                self.render('error.html',msg=u'Email 不存在')
+            else:
+                m = hashlib.md5()
+                m.update(password)
+                sign = m.hexdigest()
+                if user[0]['password'] != sign:
+                    self.render('error.html',msg=u'密码错误')
+
+                else:
+                    self.set_secure_cookie("account", email)
+                    if user[0]['isSupper']:
+                        self.redirect('/admin')
+                    else:
+                        self.redirect('/ucenter/list')
+
+
+
+class LogoutHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.clear_cookie("account")
+        #self.clear_cookie("email")
+        self.redirect('/')
+
+
+if __name__ == "__main__":
+    tornado.options.parse_command_line()
+    settings = {
+        "cookie_secret": "bZJc2sWbQLKos6GkHn/VB9oXwQt8S0R0kRvJ5/xJ89E=",
+        "xsrf_cookies": True,
+        'login_url':'/account/login'
+    }
+    app = tornado.web.Application(
+        handlers=[(r"/", IndexHandler),
+                  (r'/blog/$', BlogHandler),
+                  (r'/t/([%a-fA-F0-9]+|\w+)/$', BlogTagsHandler),
+                  (r'/c/(\w+)/$', BlogCategoryHandler),
+                  (r'/a/(\w+)', ArticleDetailHandler),
+                  (r'/ucenter/post$', PostArticleHandler),
+                  (r'/ucenter/edit/(\w+)', EditArticleHandler),
+                  (r'/ucenter/list$', AuthorArticleListHandler),
+                  (r'/account/reg$', RegHandler),
+                  (r'/account/login$', LoginHandler),
+                  (r'/account/logout$', LogoutHandler),
+                  (r'/admin', AdminHandler),
+                  (r'/auditUser/', AuditUserHandler),
+                  (r".*", BaseHandler),
+                  ],
+        template_path=os.path.join(os.path.dirname(__file__), "templates"),
+        static_path=os.path.join(os.path.dirname(__file__), "static"),
+        debug=True,
+        **settings
+    )
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
