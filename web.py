976a006b1cf01d2325fea5b375e0f383f46db085
zhaoyoucai <zhaoyc128@163.com>
2017-09-15 09:58:05 +0800

    友情链接管理页面

--- web.py
+++ web.py
@@ -83,7 +83,7 @@ class BlogHandler(BaseHandler):
 
 
         tags = db.tags.find().limit(50)
-        links = db.links.find().limit(20)
+        links = db.links.find({'showType':1}).limit(20)
         cateList = db.category.find()
 
         cateData = dict()
@@ -146,7 +146,7 @@ class BlogCategoryHandler(BaseHandler):
 
 
         tags = db.tags.find().limit(50)
-        links = db.links.find().limit(20)
+        links = db.links.find({'showType':1}).limit(20)
         cateList = db.category.find()
 
         cateData = dict()
@@ -204,7 +204,7 @@ class BlogTagsHandler(BaseHandler):
 
 
         tags = db.tags.find().limit(50)
-        links = db.links.find().limit(20)
+        links = db.links.find({'showType':1}).limit(20)
         cateList = db.category.find()
 
         cateData = dict()
@@ -488,6 +488,57 @@ class AuditUserHandler(BaseHandler):
             self.render('error.html',msg=u'非法操作！')
 
 
+class AdminLinksHandler(BaseHandler):
+    @tornado.web.authenticated
+    def get(self):
+        user = user=self.current_user
+        mongo = MongoCase()
+        mongo.connect()
+        client = mongo.client
+        db = client.pyblog
+        userInfo = db.user.find({"account":user})
+        if user and userInfo.count():
+            if userInfo[0]['isSupper']:
+                linkList = db.links.find()
+
+                self.render('admin-link.html',linkList=linkList,userInfo=userInfo[0])
+            else:
+                self.redirect('/')
+        else:
+            self.clear_cookie('account')
+            self.render('error.html',msg=u'非法用户！')
+
+
+class AuditLinksHandler(BaseHandler):
+    @tornado.web.authenticated
+    def get(self):
+        user = user=self.current_user
+        action = self.get_argument('action','')
+        id = self.get_argument('id','')
+        mongo = MongoCase()
+        mongo.connect()
+        client = mongo.client
+        db = client.pyblog
+        userInfo = db.user.find({'account':user})
+        if action != '' and id != ''and userInfo.count>0:
+            if userInfo[0]['isSupper']:
+                if action == 'delete':
+                    db.links.remove({'_id':ObjectId(id)})
+                elif action == 'deActive':
+                    db.links.update({'_id':ObjectId(id)},{'$set':{'isCheck':0,'showType':0}})
+
+                elif action == 'active':
+                    db.links.update({'_id':ObjectId(id)},{'$set':{'isCheck':1,'showType':1}})
+                elif action == 'audit':
+                    db.user.update({'_id':ObjectId(id)},{'$set':{'showType':1}})
+                self.redirect('/admin/links')
+            else:
+                self.render('error.html',msg=u'无权限删除！')
+        else:
+            self.render('error.html',msg=u'非法操作！')
+
+
+
 
 class AddLinkHandler(BaseHandler):
 
@@ -667,6 +718,8 @@ if __name__ == "__main__":
                   (r'/admin', AdminHandler),
                   (r'/admin/upload/', UploadHandler),
                   (r'/auditUser/', AuditUserHandler),
+                  (r'/admin/links', AdminLinksHandler),
+                  (r'/auditLink/', AuditLinksHandler),
                   (r'/site/addlink', AddLinkHandler),
                   (r".*", BaseHandler),
                   ],
