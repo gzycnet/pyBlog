9f12c89214aced53e7be0f7af4ba214226b83c25
zhaoyoucai <zhaoyc128@163.com>
2017-12-31 14:38:54 +0800

    归档

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -7,6 +7,8 @@ urls = [
     (r't/([%a-fA-F0-9]+|[\s\S]+|\w+)/$', BlogTagsHandler),
     (r'c/(\w+)/$', BlogCategoryHandler),
     (r'a/(\w+)/', ArticleDetailHandler),
+    #(r'archive/([%a-fA-F0-9]+|[\s\S]+|\w+)/$', BlogArchiveHandler),
+    (r'archive/(\d{4}-\d{2})/$', BlogArchiveHandler),
     (r'comment/$', CommentHandler),
     (r'addlink/$', AddLinkHandler),
 ]
